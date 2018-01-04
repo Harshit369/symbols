98825b1d54023291af8bdb9fe624db7bd05cce85
Harshit Thukral <h.thakral369@gmail.com>
2018-01-04 23:59:42 +0530

    Init Symbols + Readme
    Add readme to new symbol syntax

---
+++ new/index.js
@@ -0,0 +1,14 @@
+// creating a new symbol is easy
+Symbol(); // creates a new object of type symbol => [object Symbol] { ... }
+console.log(typeof Symbol()); // "symbol"
+console.log(typeof Symbol() === 'symbol'); // true
+
+//Defining or debugging using symbols
+
+// Symbols can be given definitions
+console.log(Symbol('me')) // [object Symbol] { ... }
+console.log(Symbols('zlatan').toString()) // "Symbol(zlatan)"
+
+/* Two symbols with same definitions can never be equal
+(there are methods to do such things, bear with me here) */
+console.log(Symbol('zlatan') === Symbol('zlatan')) //false
